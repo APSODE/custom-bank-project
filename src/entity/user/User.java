58cfdd74f15eda76f9dc4845a5fe4b866ec4d4e9
APSODE <2gunbo74@gmail.com>
2023-08-19 20:17:38 +0900

    User Class 생성자 및 객체 생성 매소드 제작

--- src/entity/user/User.java
+++ src/entity/user/User.java
@@ -1,5 +1,19 @@
 package entity.user;
 
 public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static User CreateObject(String name, int age) {
+        return new User(
+                name,
+                age
+        );
+    }
 
 }
