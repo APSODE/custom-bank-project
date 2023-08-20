855b103ab2a5e72f479b21cdc7075bd04b232638
APSODE <2gunbo74@gmail.com>
2023-08-20 20:04:16 +0900

    UserAccount 생성자, 필드 및 getter & setter 제작

--- src/entity/user/UserAccount.java
+++ src/entity/user/UserAccount.java
@@ -1,5 +1,52 @@
 package entity.user;
 
 public class UserAccount {
+    private User userObject;
+    private String id;
+    private String pw;
 
+    public UserAccount(User userObject, String id, String pw) {
+        this.userObject = userObject;
+        this.id = id;
+        this.pw = pw;
+    }
+
+    public static UserAccount CreateObject(User userObject, String id, String pw) {
+        return new UserAccount(
+                userObject,
+                id,
+                pw
+        );
+    }
+
+    public User getUserObject() {
+        return userObject;
+    }
+
+    public void setUserObject(User userObject) {
+        this.userObject = userObject;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    @Override
+    public String toString() {
+        return "User Data\n" + this.userObject.toString() + "\n\n" +
+                "User ID : " + this.id + "\n" +
+                "User PW : " + this.pw + "\n";
+    }
 }
