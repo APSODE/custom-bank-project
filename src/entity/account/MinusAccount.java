5f72b81fda98acfbbbbd35121eb600ad83597e5c
APSODE <2gunbo74@gmail.com>
2023-08-29 18:25:07 +0900

    생성자 추가 및 객체 생성 메소드 추가, repayment기능 구현

--- src/entity/account/MinusAccount.java
+++ src/entity/account/MinusAccount.java
@@ -1,5 +1,7 @@
 package entity.account;
 
+import entity.user.UserAccount;
+
 public class MinusAccount extends Account {
     private long credit;
     private long loan;
@@ -7,16 +9,27 @@ public class MinusAccount extends Account {
     private long interest;
     private double rate;
 
-    public MinusAccount(long credit, long loan, long maxRepayment, long interest, double rate) {
+    public MinusAccount(UserAccount userAccount) {
+        super(userAccount);
+        this.credit = 0;  //한도
+        this.loan = 0;  //대출액
+        this.maxRepayment = 0;  // 상환 한도
+        this.interest = 0;  // 이자
+        this.rate = 0;  //이자율
+    }
+
+    public MinusAccount(UserAccount userAccount, long credit, long loan, long maxRepayment, long interest, double rate) {
+        super(userAccount);
         this.credit = credit;  //한도
         this.loan = loan;  //대출액
-        this.maxRepayment = maxRepayment;  // 상환
+        this.maxRepayment = maxRepayment;  // 상환 한도
         this.interest = interest;  // 이자
         this.rate = rate;  //이자율
     }
 
-    public static MinusAccount CreateObject(long credit, long loan, long maxRepayment, long interest, double rate) {
+    public static MinusAccount CreateObject(UserAccount userAccount, long credit, long loan, long maxRepayment, long interest, double rate) {
         return new MinusAccount(
+                userAccount,
                 credit,
                 loan,
                 maxRepayment,
@@ -25,9 +38,49 @@ public class MinusAccount extends Account {
         );
     }
 
-    public boolean repayment() {
-        // 현재 기능 미완성
-        return true;
+    public static MinusAccount CreateObject(UserAccount userAccount) {
+        return new MinusAccount(userAccount);
+    }
+
+    public boolean isPossibleRepayment(long amount) {
+        // 1회 상환한도인 maxRepayment값보다 현재 상환 시도 중인 상환액인 amount가 작거나 같아야함.
+        return this.maxRepayment >= amount;
+    }
+
+    public boolean isLargerThanLoan(long amount) {
+        // 현재 시도 중인 상환을 기준으로 상환액이 총 대출액을 초과했는지 확인
+        return this.loan > amount;
+    }
+
+    public boolean repayment(long amount) {
+        if (isPossibleRepayment(amount)) {
+            if (isLargerThanLoan(amount)) {
+                this.loan -= amount;
+            } else {
+                // 대출액을 초과한 상환액
+                long repaymentRemainder = amount - this.loan;
+
+                // loan이 amount보다 작으므로 연산을 진행하지 않고 0으로 설정
+                this.loan = 0;
+
+                // 남은 상환액을 계좌에 다시 추가한다.
+                // 해당 라인은 boolean 리턴으로 정상작동 여부를 확인가능
+                // 따라서 정상작동 검증 로직이 추가가 필요한경우 이를 이용하여야함.
+                super.depositByOverRepayment(repaymentRemainder);
+
+            }
+
+            // 상환을 진행한 금액만큼 계좌에서 차감
+            // 해당 라인은 boolean 리턴으로 정상작동 여부를 확인가능
+            // 따라서 정상작동 검증 로직이 추가가 필요한경우 이를 이용하여야함.
+            super.withdrawByRepayment(amount);
+
+            return true;
+
+        } else {
+            return false;
+        }
+
     }
 
     public void revolving() {
