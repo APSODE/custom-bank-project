42ae211cb1bc5b62954dbbb32405c47c15604c25
Duguda_Study <mjo3482@gmail.com>
2023-08-29 19:41:05 +0900

    Error 메세지 추가

--- src/controller/Judger.java
+++ src/controller/Judger.java
@@ -3,14 +3,18 @@ package controller;
 import entity.user.User;
 import entity.user.UserAccount;
 
+import java.util.concurrent.ExecutionException;
+
 public class Judger {
-    public static boolean isPossibleToWithdraw(long balance, long amount)
+    public static boolean isPossibleToWithdraw(long balance, long amount) throws BalanceException
     {
         if (amount > 0)
         {
-            return balance - amount >= 0;
+            if(balance - amount >= 0)
+                return true;
+            else throw new BalanceException("잔액 부족");
         }
-        return false;
+        throw new IllegalArgumentException("음수인 값 입력");
     }
     public static boolean isPossibleToDeposit(long amount)
     {
@@ -19,5 +23,13 @@ public class Judger {
     public static boolean isRightPw(UserAccount userAccount, String pw)
     {
         return pw.equals(userAccount.getPw());
+    }
+
+    static class BalanceException extends Exception
+    {
+        public BalanceException(String message)
+        {
+            super(message);
+        }
     }
 }
--- src/entity/account/InstallmentAccount.java
+++ src/entity/account/InstallmentAccount.java
@@ -5,5 +5,9 @@ import java.time.LocalDate;
 public class InstallmentAccount {
     private float rate;
     private LocalDate dueDate;
+    public InstallmentAccount(LocalDate dueDate)
+    {
+        this.dueDate = dueDate;
+    }
 
 }
