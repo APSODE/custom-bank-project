27485e69ec40177cacb6f6d0a285b6139a0e3769
APSODE <2gunbo74@gmail.com>
2023-08-31 07:31:45 +0900

    직렬화를 위한 Serializable 인터페이스 상속

--- src/main/java/entity/user/User.java
+++ src/main/java/entity/user/User.java
@@ -1,6 +1,8 @@
 package entity.user;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private String name;
     private int age;
     private String phoneNumber;
