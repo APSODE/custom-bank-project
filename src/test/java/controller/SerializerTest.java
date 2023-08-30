8470a76cf834a26ce36d34372f606bc501bd27d2
APSODE <2gunbo74@gmail.com>
2023-08-31 06:16:12 +0900

    Create Serializer Unit Test File

---
+++ src/test/java/controller/SerializerTest.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SerializerTest {
+
+    @Test
+    void saveObject() {
+    }
+
+    @Test
+    void loadObject() {
+    }
+}
