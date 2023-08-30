9b1adec3e45adf756f479de937650701c4fc7610
APSODE <2gunbo74@gmail.com>
2023-08-31 02:53:56 +0900

    주석 수정

--- src/controller/Serializer.java
+++ src/controller/Serializer.java
@@ -4,6 +4,8 @@ import java.io.*;
 
 public class Serializer <T> {
     /*
+    동적 타입 캐스팅을 위한 제네릭 문법 사용.
+
     제네릭 타입은 컴파일 과정에서만 타입 데이터를 저장하고 컴파일 이후에는 Object를 타입으로 가지므로
     역직렬화 과정에서 타입 캐스팅 관련 문제가 발생할 가능성이 존재한다.
     따라서 Serializer클래스의 생성자의 파라미터로 추가로 타입 데이터를 전달받아서 객체 내부 상수 필드에 타입을 저장하여
@@ -48,6 +50,7 @@ public class Serializer <T> {
             loadedObject = this.targetObjectType.cast(
                     ois.readObject()
             );
+
         } catch (ClassNotFoundException CNFE) {
             Printer.print(
                     "역직렬화 대상의 클래스와 일치하는 클래스를 찾을 수 없습니다.\n" +
