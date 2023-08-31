c3c5b3e6e4af07e9d790218aa4036cb51be72437
APSODE <2gunbo74@gmail.com>
2023-08-31 17:03:37 +0900

    Point클래스 피드백 추가 및 패드백 반영 예시 코드

--- src/main/java/entity/account/PointAccount.java
+++ src/main/java/entity/account/PointAccount.java
@@ -1,6 +1,6 @@
 package entity.account;
 
-import controller.Exceptions.PointException;
+import controller.exceptions.PointException;
 import entity.user.UserAccount;
 import controller.Judger;
 
@@ -16,6 +16,8 @@ public class PointAccount extends Account {
     }
 
     public boolean earnPoints(long amount) {
+        // 포인트 적립 과정에서 입금이랑 어떤관계가 있었는지 모르겠음
+        // 해당 if문 안의 조건식이 false여서 포인트 적립이 되지 않아도 해당 메소드는 true를 리턴함
         if (Judger.isPossibleToDeposit(amount)) {
             this.point += (long) (amount * 0.03);
         }
@@ -23,6 +25,7 @@ public class PointAccount extends Account {
     }
 
     public boolean usePoints(long amount) throws PointException {
+        // Exception을 발생시킬때 Judger에서 발생시키는 것이 아닌 해당 메소드 안에서 발생시키는 것이 좋음
         if (Judger.isPossibleToUsePoint(this.getBalance(), amount, this.point)){
             if (amount <= this.point) {
                 this.point -= amount;
@@ -32,6 +35,27 @@ public class PointAccount extends Account {
 
             }
         }
+        return true;
+    }
+
+    // usePoints 리팩토링 예시 코드
+    private boolean exampleUsePointsMethod(long amount) throws PointException {
+        if (!Judger.isOverThanMinUsePoint(this.point)) {
+            throw new PointException("포인트 사용은 5000포인트부터 사용이 가능합니다.");
+        }
+
+        if (!Judger.isShortOfBalance(this.getBalance(), amount, this.point)) {
+            throw new PointException("계좌의 잔액이 부족합니다.");
+        }
+
+        if (amount <= this.point) {
+            this.point -= amount;
+        } else {
+            this.setBalance(
+                    this.getBalance() + this.point - amount
+            );
+        }
+
         return true;
     }
 }
