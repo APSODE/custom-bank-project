d514a8471eff1bae803626d99efa9b1d06edafe7
APSODE <2gunbo74@gmail.com>
2023-08-31 22:57:11 +0900

    들여쓰기 수준 반영

--- src/main/java/entity/account/SavingAccount.java
+++ src/main/java/entity/account/SavingAccount.java
@@ -33,8 +33,8 @@ public class SavingAccount extends Account implements Serializable {
             // 이체금액 제한 초과시
             throw new OverWithrawLimitException("제한금액 초과");
         }
-            return super.withdraw(amount, pw);
 
+        return super.withdraw(amount, pw);
     }
 }
 
