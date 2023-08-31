126542cd5b97f5afdd25ff3b98b99132f1db6592
APSODE <2gunbo74@gmail.com>
2023-08-31 21:40:51 +0900

    OverWithrawLimitException 예외추가

---
+++ src/main/java/controller/exceptions/OverWithrawLimitException.java
@@ -0,0 +1,7 @@
+package controller.exceptions;
+
+public class OverWithrawLimitException extends Exception {
+    public OverWithrawLimitException(String errorMessage) {
+        super(errorMessage);
+    }
+}
