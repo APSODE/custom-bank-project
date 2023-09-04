9b29bb27f3af9a19964ac6c4c47368d9a7ca0b3b
APSODE <2gunbo74@gmail.com>
2023-09-04 22:06:17 +0900

    InstallmentAccount 클래스에 대한 테스트 코드 작성 완료

---
+++ src/test/java/entity/account/InstallmentAccountTest.java
@@ -0,0 +1,53 @@
+package entity.account;
+
+import controller.exceptions.InvalidPhoneNumberFormat;
+import entity.user.User;
+import entity.user.UserAccount;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InstallmentAccountTest {
+    private InstallmentAccount testInstallmentAccount;
+    private UserAccount testUserAccount;
+    private String testUserAccountPw = "testPw";
+
+    @BeforeEach
+    void createTestObject() throws InvalidPhoneNumberFormat {
+        this.testUserAccount = UserAccount.CreateObject(
+                User.CreateObject(
+                        "test user",
+                        20,
+                        "010-1234-5678"
+                ),
+                "test_id",
+                this.testUserAccountPw
+        );
+    }
+
+    @Test
+    void dueProcess() {
+        // given
+        LocalDate yesterday = LocalDate.now().minusDays(1);
+
+        InstallmentAccount testInstallmentAccount = InstallmentAccount.CreateObject(
+                yesterday,
+                0.1,
+                this.testUserAccount
+        );
+        long beforeBalance = testInstallmentAccount.getBalance();
+        long expectedBalance = beforeBalance + (long) (beforeBalance * testInstallmentAccount.getRate());
+
+
+        // when
+        testInstallmentAccount.dueProcess(LocalDate.now());
+
+
+        // then
+        assertEquals(expectedBalance, testInstallmentAccount.getBalance());
+    }
+}
