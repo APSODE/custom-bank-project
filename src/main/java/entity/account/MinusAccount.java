6f74800734a7122796957a2220126d11e9a90128
APSODE <2gunbo74@gmail.com>
2023-09-05 08:40:51 +0900

    interimRepayment 메소드 구현 변경

--- src/main/java/entity/account/MinusAccount.java
+++ src/main/java/entity/account/MinusAccount.java
@@ -1,14 +1,9 @@
 package entity.account;
 
-import controller.exceptions.BalanceException;
-import controller.exceptions.InvalidPasswordException;
-import controller.exceptions.NegativeAmountException;
-import controller.exceptions.RepaymentException;
+import controller.exceptions.*;
 import controller.Judger;
-import controller.Printer;
 import entity.user.UserAccount;
 
-import java.io.IOException;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -82,19 +77,23 @@ public class MinusAccount extends Account implements Serializable {
     }
 
     // IOException은 Account 클래스의 예외 발생 부분을 수정하면 throws에서 제거
-    public boolean interimRepayment(long amount, String pw) throws BalanceException, RepaymentException, InvalidPasswordException, NegativeAmountException {
-        if (!Judger.isRightPw(super.getUserAccount(), pw)) {
-            throw new InvalidPasswordException("비밀번호 오류, 비밀번호를 재확인하고 다시 시도하십시오.");
-        }
-
+    public boolean interimRepayment(long amount, String pw) throws RepaymentException, InvalidPasswordException, NegativeAmountException, NoLongerAvailableMinusWithdraw, NotExistLoanAmount {
         if (!Judger.isSmallerThanMaxRepayment(this.maxRepayment, amount)) {
             throw new RepaymentException("1회 상환한도를 초과하는 상환액입니다. 상환액을 재입력하여주십시오.");
+        }
+
+        if (!Judger.isExistLoanAmount(this.loan)) {
+            throw new NotExistLoanAmount("현재 대출액이 존재하지 않습니다.");
+        }
+
+        if (!Judger.isRightPw(this.getUserAccount(), pw)) {
+            throw new InvalidPasswordException("비밀번호가 옳바르지 않습니다.");
         }
 
         // 상환을 진행한 금액만큼 계좌에서 차감
         // 해당 라인은 boolean 리턴으로 정상작동 여부를 확인가능
         // 따라서 정상작동 검증 로직이 추가가 필요한경우 이를 이용하여야함.
-        super.withdraw(amount, pw);
+        this.setBalance(this.getBalance() - amount);
 
         if (Judger.isLargerThanLoan(this.loan, amount)) {
             // 대출액을 초과한 상환액
@@ -106,7 +105,7 @@ public class MinusAccount extends Account implements Serializable {
             // 남은 상환액을 계좌에 다시 추가한다.
             // 해당 라인은 boolean 리턴으로 정상작동 여부를 확인가능
             // 따라서 정상작동 검증 로직이 추가가 필요한경우 이를 이용하여야함.
-            super.deposit(repaymentRemainder, pw);
+            this.setBalance(this.getBalance() + repaymentRemainder);
 
         } else {
             this.loan -= amount;
