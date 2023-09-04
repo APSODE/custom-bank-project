36a9138cdb62c076a1b399a8a64e18fe2d810b1a
APSODE <2gunbo74@gmail.com>
2023-09-05 08:36:07 +0900

    MinusAccount 출금한도관련 예외 클래스

---
+++ src/main/java/controller/exceptions/NoLongerAvailableMinusWithdraw.java
@@ -0,0 +1,7 @@
+package controller.exceptions;
+
+public class NoLongerAvailableMinusWithdraw extends Exception {
+    public NoLongerAvailableMinusWithdraw(String errorMessage) {
+        super(errorMessage);
+    }
+}
