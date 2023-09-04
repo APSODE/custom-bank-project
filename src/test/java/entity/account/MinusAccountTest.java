517b3a3f96da041bce7c534c37abb7210a6f9466
APSODE <2gunbo74@gmail.com>
2023-09-05 08:42:25 +0900

    MinusAccount에 대한 단위테스트 코드 추가 (미완성)

---
+++ src/test/java/entity/account/MinusAccountTest.java
@@ -0,0 +1,104 @@
+package entity.account;
+
+import controller.exceptions.InvalidPasswordException;
+import controller.exceptions.NoLongerAvailableMinusWithdraw;
+import controller.exceptions.NotExistLoanAmount;
+import controller.exceptions.RepaymentException;
+import entity.user.UserAccount;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+class MinusAccountTest {
+    private final UserAccount testUserAccount = UserAccount.CreateTestObject();
+    private final MinusAccount testMinusAccount = MinusAccount.CreateObject(
+            this.testUserAccount
+    );
+
+    @BeforeEach
+    void minusAccountTestSetting() {
+        this.testMinusAccount.setBalance(100000);
+        this.testMinusAccount.setCredit(100000);
+        this.testMinusAccount.setMaxRepayment(10000);
+        this.testMinusAccount.setRate(0.1);
+        this.testMinusAccount.setInterest();
+    }
+
+    @DisplayName("중도 상환 테스트")
+    @Test
+    void interimRepayment() {
+        assertThatCode(() -> {
+            this.testMinusAccount.setLoan(100);
+            this.testMinusAccount.interimRepayment(
+                    100,
+                    this.testUserAccount.getPw()
+            );
+        }).doesNotThrowAnyException();
+
+        assertThatThrownBy(() -> {
+            this.testMinusAccount.interimRepayment(
+                    this.testMinusAccount.getMaxRepayment() + 1,
+                    this.testUserAccount.getPw()
+            );
+        }).isInstanceOf(RepaymentException.class);
+
+        assertThatThrownBy(() -> {
+            this.testMinusAccount.setLoan(0);
+            this.testMinusAccount.interimRepayment(
+                    100,
+                    this.testUserAccount.getPw()
+            );
+        }).isInstanceOf(NotExistLoanAmount.class);
+
+    }
+
+    @DisplayName("정상 상환 테스트")
+    @Test
+    void repayment() {
+        assertThatCode(() -> {
+            this.testMinusAccount.repayment(
+                    this.testUserAccount.getPw()
+            );
+        }).doesNotThrowAnyException();
+
+        assertThatThrownBy(() -> {
+            this.testMinusAccount.setLoan(10000);
+            this.testMinusAccount.setBalance(this.testMinusAccount.getLoan() - 1);
+            this.testMinusAccount.repayment(
+                    this.testUserAccount.getPw()
+            );
+        }).isInstanceOf(RepaymentException.class);
+
+        assertThatThrownBy(() -> {
+            this.testMinusAccount.repayment(
+                    "invalid password"
+            );
+        }).isInstanceOf(InvalidPasswordException.class);
+    }
+
+    @Test
+    void revolving() {
+    }
+
+    @DisplayName("마이너스 계좌 출금 테스트")
+    @Test
+    void withdrawInMinusAccount() {
+        assertThatCode(() -> {
+            testMinusAccount.withdrawInMinusAccount(
+                    100,
+                    this.testUserAccount.getPw()
+            );
+        }).doesNotThrowAnyException();
+
+        assertThatThrownBy(() -> {
+            testMinusAccount.withdrawInMinusAccount(
+                    99901,
+                    this.testUserAccount.getPw()
+            );
+        }).isInstanceOf(NoLongerAvailableMinusWithdraw.class);
+    }
+}
