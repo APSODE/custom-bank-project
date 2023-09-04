bf8846b5ed3c996b2e8db194b8468bbef1da1bae
APSODE <2gunbo74@gmail.com>
2023-09-05 08:34:14 +0900

    Judger 클래스 조건 검증 메소드 이름 변경으로 인한 수정

--- src/main/java/entity/account/SavingAccount.java
+++ src/main/java/entity/account/SavingAccount.java
@@ -1,14 +1,12 @@
 package entity.account;
 
 import controller.Judger;
-import controller.Printer;
 import controller.exceptions.BalanceException;
 import controller.exceptions.InvalidPasswordException;
 import controller.exceptions.NegativeAmountException;
 import controller.exceptions.OverWithrawLimitException;
 import entity.user.UserAccount;
 
-import java.io.IOException;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -37,7 +35,7 @@ public class SavingAccount extends Account implements Serializable {
     }
 
     public boolean withdrawWithLimit(long amount, String pw) throws OverWithrawLimitException, BalanceException, NegativeAmountException, InvalidPasswordException {
-        if (!Judger.isSmallerThanAmount(this.getLimit(), amount)) {
+        if (!Judger.isNotOverTheLimit(this.getLimit(), amount)) {
             // 이체금액 제한 초과시
             throw new OverWithrawLimitException("제한금액 초과");
         }
