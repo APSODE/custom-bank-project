ac39f4dc1580203e66661d01c1eae010c7a2340f
APSODE <2gunbo74@gmail.com>
2023-09-06 10:55:59 +0900

    PointAccount 클래스에 대한 단위테스트 코드 작성 완료

--- src/test/java/entity/account/PointAccountTest.java
+++ src/test/java/entity/account/PointAccountTest.java
@@ -7,26 +7,41 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class PointAccountTest {
     private final UserAccount testUserAccount = UserAccount.CreateTestObject();
-    private final PointAccount testPointAccount = PointAccount.CreateObject(
-            this.testUserAccount
-    );
-
-    @BeforeEach
-    void pointAccountTestSetting() {
-        this.testPointAccount.setBalance(1000000);
-    }
 
     @DisplayName("포인트 적립 테스트")
     @Test
     void earnPoints() {
+        // given
+        PointAccount testPointAccount = PointAccount.CreateObject(
+                this.testUserAccount
+        );
 
+        // when
+        testPointAccount.earnPoints(10000);
+
+        // then
+        assertEquals(300, testPointAccount.getPoint());
     }
 
     @DisplayName("포인트 사용 테스트")
     @Test
     void withdrawWithPoint() {
+        PointAccount testPointAccount = PointAccount.CreateObject(
+                this.testUserAccount
+        );
+
+        assertThatCode(() -> {
+            testPointAccount.setBalance(1000000);
+            testPointAccount.setPoint(10000);
+
+            testPointAccount.withdrawWithPoint(
+                    7000,
+                    this.testUserAccount.getPw()
+            );
+        }).doesNotThrowAnyException();
     }
 }
