4a2c34dede690169aff778fca3fa678a502468d2
Duguda_Study <mjo3482@gmail.com>
2023-09-21 22:38:15 +0900

    Main 추가, Menu 수정

--- src/main/java/boundary/Menu.java
+++ src/main/java/boundary/Menu.java
@@ -30,39 +30,17 @@ public class Menu {
     }
 
     public void accountInquiry(String id, String pw, String type) throws IOException, NoTypeException {
-        if (type.equals("Account"))
-        {
-            String fileName = "\\account\\Account"+id+".cbp";
-            Serializer<Account> serializer = new Serializer<>(Account.class);
-            this.selectAccount = serializer.loadObject(fileName);
-        }
-        else if (type.equals("Installment"))
-        {
-            String fileName = "\\account\\Installment"+id+".cbp";
-            Serializer<InstallmentAccount> serializer = new Serializer<>(InstallmentAccount.class);
-            this.selectAccount = serializer.loadObject(fileName);
-        }
-        else if (type.equals("Minus"))
-        {
-            String fileName = "\\account\\Minus"+id+".cbp";
-            Serializer<MinusAccount> serializer = new Serializer<>(MinusAccount.class);
-            this.selectAccount = serializer.loadObject(fileName);
-        }
-        else if (type.equals("Point"))
-        {
-            String fileName = "\\account\\Point"+id+".cbp";
-            Serializer<PointAccount> serializer = new Serializer<>(PointAccount.class);
-            this.selectAccount = serializer.loadObject(fileName);
-        }
-        else if (type.equals("Saving"))
-        {
-            String fileName = "\\account\\Minus"+id+".cbp";
-            Serializer<SavingAccount> serializer = new Serializer<>(SavingAccount.class);
-            this.selectAccount = serializer.loadObject(fileName);
-        }
-        else {
-            throw new NoTypeException("존재하지 않는 형식입니다.");
-        }
+        final String FILE_PATH = "\\account"; final String FILE_EXTENSION = ".cbp";
+        String filename = FILE_PATH + type + id + FILE_EXTENSION;
+        Serializer serializer = switch (type) {
+            case "Account" -> new Serializer<>(Account.class);
+            case "Installment" -> new Serializer<>(InstallmentAccount.class);
+            case "Minus" -> new Serializer<>(MinusAccount.class);
+            case "Point" -> new Serializer<>(PointAccount.class);
+            case "Saving" -> new Serializer<>(SavingAccount.class);
+            default -> throw new NoTypeException("존재하지 않는 형식입니다.");
+        };
+        this.selectAccount = (Account) serializer.loadObject(filename);
     }
 
     // 테스트 코드 동작용으로 임시로 throws작성
@@ -79,11 +57,7 @@ public class Menu {
         try {
             this.selectAccount.withdraw(amount, pw);
         }
-        catch (IllegalArgumentException e)
-        {
-            Printer.print(e.getMessage());
-        }
-        catch (BalanceException e)
+        catch (IllegalArgumentException | BalanceException e)
         {
             Printer.print(e.getMessage());
         }
